56a5713af2df57e444e4c377f3a4eb723305ee3a
Chang Yuan Shen <wolf103057@gmail.com>
2020-04-18 14:44:53 +0800

    Updated by user1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
   public static void main(String[] args){
-    int i=1;
+    int i=2;
     float f=4.0f;
     System.out.println("Hello world!");
     System.out.println("Written again in branch-1");
